0469d2be62dfbf9519acee366c4309ff822e018a
tjudice <tjudice@umich.edu>
2023-08-20 19:41:03 -0500

    seperate orderbook levels due to differing response types

--- exchange/coinbase/client_test.go
+++ exchange/coinbase/client_test.go
@@ -1,5 +1,13 @@
 package coinbase_test
 
+import (
+	"context"
+	"log"
+	"testing"
+
+	"github.com/tjudice/ethutil/exchange/coinbase"
+)
+
 // func TestGetMarkets(t *testing.T) {
 // 	cl := coinbase.NewClient()
 // 	res, err := cl.GetMarkets(context.TODO())
@@ -11,9 +19,9 @@ package coinbase_test
 // 	}
 // }
 
-// func TestGetbook(t *testing.T) {
+// func TestGetbook12(t *testing.T) {
 // 	cl := coinbase.NewClient()
-// 	book, err := cl.GetMarketBook(context.TODO(), "btc-usd", 2)
+// 	book, err := cl.GetMarketBookLevel1(context.TODO(), "btc-usd")
 // 	if err != nil {
 // 		t.Fatal(err)
 // 	}
@@ -21,3 +29,14 @@ package coinbase_test
 // 		log.Printf("%+v\n", b)
 // 	}
 // }
+
+func TestGetbook3(t *testing.T) {
+	cl := coinbase.NewClient()
+	book, err := cl.GetMarketBookLevel3(context.TODO(), "btc-usd")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, b := range book.Bids {
+		log.Printf("%+v\n", b)
+	}
+}
--- exchange/coinbase/markets.go
+++ exchange/coinbase/markets.go
@@ -52,18 +52,20 @@ type Order struct {
 	NumOrders int     `json:"num_orders"`
 }
 
+type OrderbookWrapper struct {
+	Sequence    int64     `json:"sequence"`
+	Time        time.Time `json:"time"`
+	AuctionMode bool      `json:"auction_mode"`
+	Auction     *string   `json:"auction"`
+}
+
 type Orderbook struct {
-	Sequence    int64            `json:"sequence"`
-	Time        time.Time        `json:"time"`
-	AuctionMode bool             `json:"auction_mode"`
-	Auction     *string          `json:"auction"`
-	Bids        OrderMarshalling `json:"bids"`
-	Asks        OrderMarshalling `json:"asks"`
+	OrderbookWrapper
+	Bids OrderMarshalling `json:"bids"`
+	Asks OrderMarshalling `json:"asks"`
 }
 
 type OrderMarshalling []*Order
-
-var _ json.Unmarshaler = &OrderMarshalling{}
 
 func (o *OrderMarshalling) UnmarshalJSON(bts []byte) error {
 	var orderArray []json.RawMessage
@@ -85,8 +87,50 @@ func (o *OrderMarshalling) UnmarshalJSON(bts []byte) error {
 	return nil
 }
 
-func (c *Client) GetMarketBook(ctx context.Context, marketId string, level int) (*Orderbook, error) {
-	return jsonhttp.Get[*Orderbook](ctx, c.cl, makeBookURL(marketId, level), nil)
+func (c *Client) GetMarketBookLevel1(ctx context.Context, marketId string) (*Orderbook, error) {
+	return jsonhttp.Get[*Orderbook](ctx, c.cl, makeBookURL(marketId, 1), nil)
+}
+
+func (c *Client) GetMarketBookLevel2(ctx context.Context, marketId string) (*Orderbook, error) {
+	return jsonhttp.Get[*Orderbook](ctx, c.cl, makeBookURL(marketId, 2), nil)
+}
+
+type OrderbookLevel3 struct {
+	OrderbookWrapper
+	Bids Order3Marshalling `json:"bids"`
+	Asks Order3Marshalling `json:"asks"`
+}
+
+type Order3Marshalling []*Order3
+
+type Order3 struct {
+	Amount  float64 `json:"amount"`
+	Price   float64 `json:"price"`
+	OrderID string  `json:"order_id"`
+}
+
+func (o *Order3Marshalling) UnmarshalJSON(bts []byte) error {
+	var orderArray []json.RawMessage
+	err := json.Unmarshal(bts, &orderArray)
+	if err != nil {
+		return err
+	}
+	orders := make([]*Order3, 0, len(orderArray))
+	for _, currOrder := range orderArray {
+		nextOrder := &Order3{}
+		orderVars := []interface{}{&FloatStringWrapper{&nextOrder.Price}, &FloatStringWrapper{&nextOrder.Amount}, &nextOrder.OrderID}
+		err := json.Unmarshal(currOrder, &orderVars)
+		if err != nil {
+			return err
+		}
+		orders = append(orders, nextOrder)
+	}
+	*o = orders
+	return nil
+}
+
+func (c *Client) GetMarketBookLevel3(ctx context.Context, marketId string) (*OrderbookLevel3, error) {
+	return jsonhttp.Get[*OrderbookLevel3](ctx, c.cl, makeBookURL(marketId, 3), nil)
 }
 
 func makeBookURL(marketId string, level int) string {
