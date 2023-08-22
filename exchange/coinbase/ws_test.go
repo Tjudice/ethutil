0195092808c4a5f23218bf79632d1c83408a80e5
tjudice <tjudice@umich.edu>
2023-08-22 01:43:06 -0500

    uncomment tests

--- exchange/coinbase/ws_test.go
+++ exchange/coinbase/ws_test.go
@@ -19,100 +19,100 @@ func getWsClient() *coinbase.Client {
 	return coinbase.NewClient(acc)
 }
 
-// func TestSubscribeFull(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"full"})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		for {
-// 			x := <-conn.C()
-// 			log.Println(x)
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// }
+func TestSubscribeFull(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"full"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		for {
+			x := <-conn.C()
+			log.Println(x)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
 
-// func TestSubscribeTicker(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"ticker"})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		for {
-// 			x := <-conn.C()
-// 			log.Println(x)
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// }
+func TestSubscribeTicker(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"ticker"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		for {
+			x := <-conn.C()
+			log.Println(x)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
 
-// func TestSubscribeStatus(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"status"})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		for {
-// 			x := <-conn.C()
-// 			log.Printf("%+v", x)
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// }
+func TestSubscribeStatus(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), []string{"MXC-USD"}, []any{"status"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		for {
+			x := <-conn.C()
+			log.Printf("%+v", x)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
 
-// func TestSubscribeLevel3(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), []string{"BTC-USD"}, []any{"level3"})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	processed := 0
-// 	go func() {
-// 		for {
-// 			<-conn.C()
-// 			processed += 1
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// 	log.Println(processed)
-// }
+func TestSubscribeLevel3(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), []string{"BTC-USD"}, []any{"level3"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	processed := 0
+	go func() {
+		for {
+			<-conn.C()
+			processed += 1
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	log.Println(processed)
+}
 
-// func TestSubscribeTickerBatch(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), []string{"BTC-USD"}, []any{"ticker_batch"})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		for {
-// 			log.Println(<-conn.C())
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// }
+func TestSubscribeTickerBatch(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), []string{"BTC-USD"}, []any{"ticker_batch"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		for {
+			log.Println(<-conn.C())
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
 
-// func TestSubscribeRFQMatches(t *testing.T) {
-// 	cl := getWsClient()
-// 	conn, err := cl.Subscribe(context.TODO(), nil, []any{
-// 		struct {
-// 			Name string `json:"name"`
-// 		}{
-// 			Name: "rfq_matches"},
-// 	})
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	go func() {
-// 		for {
-// 			log.Println(<-conn.C())
-// 		}
-// 	}()
-// 	time.Sleep(time.Minute)
-// }
+func TestSubscribeRFQMatches(t *testing.T) {
+	cl := getWsClient()
+	conn, err := cl.Subscribe(context.TODO(), nil, []any{
+		struct {
+			Name string `json:"name"`
+		}{
+			Name: "rfq_matches"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	go func() {
+		for {
+			log.Println(<-conn.C())
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
 
 func TestSusbcribeLevel2(t *testing.T) {
 	cl := getWsClient()
@@ -127,6 +127,6 @@ func TestSusbcribeLevel2(t *testing.T) {
 			cnt = cnt + 1
 		}
 	}()
-	time.Sleep(time.Minute)
+	time.Sleep(10 * time.Second)
 	log.Println(cnt)
 }
