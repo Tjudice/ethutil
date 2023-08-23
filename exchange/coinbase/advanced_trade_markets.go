befc5c8d6997905a9436a13666ecbbb29419a520
tjudice <tjudice@umich.edu>
2023-08-23 01:52:07 -0500

    progress

--- exchange/coinbase/advanced_trade_markets.go
+++ exchange/coinbase/advanced_trade_markets.go
@@ -2,6 +2,7 @@ package coinbase
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -37,13 +38,27 @@ func (c *AdvancedTradeClient) GetBestBidAsk(ctx context.Context, productIds []st
 	})
 }
 
-type AdvancedTradeOrderbook struct{}
+type OrderbookWrapperAdvancedTrade struct {
+	Pricebook *AdvancedTradeOrderbook `json:"pricebook"`
+}
+
+type AdvancedTradeOrderbook struct {
+	ProductId string    `json:"product_id"`
+	Bids      []*Tick   `json:"bids"`
+	Asks      []*Tick   `json:"asks"`
+	Time      time.Time `json:"time"`
+}
 
 const ADVANCED_TRADE_ORDERBOOK_URL = "https://api.coinbase.com/api/v3/brokerage/product_book"
 
 func (c *AdvancedTradeClient) GetOrderbook(ctx context.Context, productId string, limit int) (*AdvancedTradeOrderbook, error) {
-
-	panic("not implemented")
+	wrapper, err := http_helpers.GetJSONFn[*OrderbookWrapperAdvancedTrade](ctx, c.cl, ADVANCED_TRADE_ORDERBOOK_URL, nil, func(r *http.Request) {
+		r.URL.RawQuery, _ = http_helpers.NewURLEncoder(r.URL.Query()).Add("product_id", productId).AddIfNotDefault("limit", limit, 0).Encode()
+	})
+	if err != nil {
+		return nil, err
+	}
+	return wrapper.Pricebook, nil
 }
 
 type GetMarketParams struct {
@@ -54,14 +69,59 @@ type GetMarketParams struct {
 	ContractExpiryType ContractExpiryType
 }
 
-type AdvancedTradeMarket struct{}
+type AdvancedTradeMarket struct {
+	ProductId                 string      `json:"product_id"`
+	Price                     float64     `json:"price,string"`
+	PricePercentageChange24H  float64     `json:"price_percentage_change_24h,string"`
+	Volume24H                 float64     `json:"volume_24h,string"`
+	VolumePercentageChange24H float64     `json:"volume_percentage_change_24h,string"`
+	BaseIncrement             float64     `json:"base_increment,string"`
+	QuoteIncrement            float64     `json:"quote_increment,string"`
+	QuoteMinSize              float64     `json:"quote_min_size,string"`
+	QuoteMaxSize              float64     `json:"quote_max_size,string"`
+	BaseMinSize               float64     `json:"base_min_size,string"`
+	BaseMaxSize               float64     `json:"base_max_size,string"`
+	BaseName                  string      `json:"base_name"`
+	QuoteName                 string      `json:"quote_name"`
+	Watched                   bool        `json:"watched"`
+	IsDisabled                bool        `json:"is_disabled"`
+	New                       bool        `json:"new"`
+	Status                    string      `json:"status"`
+	CancelOnly                bool        `json:"cancel_only"`
+	LimitOnly                 bool        `json:"limit_only"`
+	PostOnly                  bool        `json:"post_only"`
+	TradingDisabled           bool        `json:"trading_disabled"`
+	AuctionMode               bool        `json:"auction_mode"`
+	ProductType               string      `json:"product_type"`
+	QuoteCurrencyId           string      `json:"quote_currency_id"`
+	BaseCurrencyId            string      `json:"base_currency_id"`
+	FcmTradingSessionDetails  interface{} `json:"fcm_trading_session_details"`
+	MidMarketPrice            string      `json:"mid_market_price"`
+	AliasTo                   []string    `json:"alias_to"`
+	BaseDisplaySymbol         string      `json:"base_display_symbol"`
+	QuoteDisplaySymbol        string      `json:"quote_display_symbol"`
+	ViewOnly                  bool        `json:"view_only"`
+	PriceIncrement            float64     `json:"price_increment,string"`
+}
 
-type AdvancedTradeMarkets struct{}
+type AdvancedTradeMarkets struct {
+	Products []json.RawMessage `json:"products"`
+}
 
 const ADVANCED_TRADE_GET_MARKETS_URL = "https://api.coinbase.com/api/v3/brokerage/products"
 
-func (c *AdvancedTradeClient) GetMarkets(ctx context.Context, params *GetMarketParams) (*AdvancedTradeMarkets, error) {
-	panic("not implemented")
+func (c *AdvancedTradeClient) GetMarkets(ctx context.Context, params *GetMarketParams) (json.RawMessage, error) {
+	return http_helpers.GetJSONFn[json.RawMessage](ctx, c.cl, ADVANCED_TRADE_GET_MARKETS_URL, nil, func(r *http.Request) {
+		if params == nil {
+			return
+		}
+		r.URL.RawQuery, _ = http_helpers.NewURLEncoder(r.URL.Query()).
+			AddIfNotDefault("limit", params.Limit, 0).
+			AddIfNotDefault("offset", params.Offset, 0).
+			AddIfNotDefault("product_type", string(params.ProductType), "").
+			Add("product_ids", lambda.SliceToAny(params.ProductIds)...).
+			AddIfNotDefault("contract_expiry_type", string(params.ContractExpiryType), "").Encode()
+	})
 }
 
 const ADVANCED_TRADE_GET_MARKET_URL = "https://api.coinbase.com/api/v3/brokerage/products/%s"
--- exchange/coinbase/advanced_trade_markets_test.go
+++ exchange/coinbase/advanced_trade_markets_test.go
@@ -15,11 +15,32 @@ func getAdvancedTradeClient2() *coinbase.AdvancedTradeClient {
 	return coinbase.NewAdvancedTradeClient(acc)
 }
 
-func TestGetBestBidAsk(t *testing.T) {
+// func TestGetBestBidAsk(t *testing.T) {
+// 	cl := getAdvancedTradeClient2()
+// 	res, err := cl.GetBestBidAsk(context.TODO(), []string{"BTC-USD", "ETH-USD"})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	log.Println(res.PriceBooks[0].Bids[0])
+// }
+
+// func TestGetOrderbook(t *testing.T) {
+// 	cl := getAdvancedTradeClient2()
+// 	res, err := cl.GetOrderbook(context.TODO(), "NEST-USD", 10)
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	log.Println(res)
+// }
+
+func TestGetMarketsAdvanced(t *testing.T) {
 	cl := getAdvancedTradeClient2()
-	res, err := cl.GetBestBidAsk(context.TODO(), []string{"BTC-USD", "ETH-USD"})
+	res, err := cl.GetMarkets(context.TODO(), &coinbase.GetMarketParams{
+		Limit:  100,
+		Offset: 50,
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Println(res.PriceBooks[0].Bids[0])
+	log.Println(string(res))
 }
